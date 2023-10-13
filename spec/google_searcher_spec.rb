33e0c2dd80829d58528c75e479310a25894e151c
ikramiste <ikrame.saadi@gmail.com>
2023-10-13 17:52:10 +0200

    spec created

---
+++ spec/google_searcher_spec.rb
@@ -0,0 +1,17 @@
+require_relative '..lib/google_searcher.rb'
+
+describe "#get_search_input_or_stop" do
+
+it "stops the program if there is no input" do
+    expect(ARVG).not_to (be_nil)
+end
+
+end
+
+describe "#build_url" do
+
+it "create an URL from an input" do 
+    expect(build_url).to eq().to eq()
+end
+
+end
